ff46ee32c9a8a9e3917fa93bbdb377dfc10c4a3e
Osman <osmanyasir.ozyilmaz@iaau.edu.kg>
2022-01-13 08:21:35 +0600

    add firebase

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -52,3 +52,5 @@ String dummyText =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. When an unknown printer took a galley.";
 
 
+
+
